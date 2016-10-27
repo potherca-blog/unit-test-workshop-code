a4c5bdfeb61aa77d8fe23b023ded87c56b747ba4
Ben Peachey <b.peachey@dealerdirect.nl>
2016-10-27 14:27:38 +0200

    CHANGE: Adds a test for when not given limit.

--- tests/WordWrapperTest.php
+++ tests/WordWrapperTest.php
@@ -16,15 +16,26 @@ class WordWrapperTest extends \PHPUnit_Framework_TestCase
         new WordWrapper(false);
     }
 
-    final public function testWordWrapperShouldWrapWordsToLimitWhenGivenLimit()
+    final public function testWordWrapperShouldOnlyWrapWordsWhenGivenLimit()
     {
         $wordWrapper = new WordWrapper();
+
+        $actual = $wordWrapper->wrap('These are some words');
+        $expected = $actual;
+
+        self::assertEquals($expected, $actual);
+    }
+
+    final public function testWordWrapperShouldWrapWordsToLimitWhenGivenLimit()
+    {
+        $wordWrapper = new WordWrapper(5);
         $actual = $wordWrapper->wrap('These are some words');
 
         $expected = <<<TXT
 These
  are 
  some 
+ 
 words
 TXT;
         self::assertEquals($expected, $actual);
