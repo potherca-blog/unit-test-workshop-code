16f16b4d252b8f677715b03665290c913b1d7d70
Ben Peachey <b.peachey@dealerdirect.nl>
2016-10-27 13:54:22 +0200

    ADD: Adds skeleton WordWrapper class.

---
+++ src/WordWrapInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace RopBot;
+
+interface WordWrapInterface
+{
+    public function __construct($p_max_length = null);
+
+    public function wrap($input);
+}
+
+/*EOF*/
---
+++ src/WordWrapper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Potherca\TestWorkshop;
+
+use RopBot\WordWrapInterface;
+
+class WordWrapper implements WordWrapInterface
+{
+
+    public function __construct($p_max_length = null)
+    {
+    }
+
+    public function wrap($input)
+    {
+    }
+}
