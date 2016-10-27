38b1417139cdb914bf64db0f01afb6a2fbd2da7b
Ben Peachey <b.peachey@dealerdirect.nl>
2016-10-27 14:11:28 +0200

    CHANGE: Adds more tests.

--- tests/WordWrapperTest.php
+++ tests/WordWrapperTest.php
@@ -4,8 +4,29 @@ namespace Potherca\TestWorkshop;
 
 class WordWrapperTest extends \PHPUnit_Framework_TestCase
 {
-    final public function testWordWrapperShouldBeInstantiatedWithoutParameter()
+    final public function testWordWrapperShouldBeInstantiatedWhenNotGivenLimit()
     {
         new WordWrapper();
+    }
+
+    final public function testWordWrapperShouldComplainWhenGivenNonIntegerAsLimit()
+    {
+        $this->expectException(\TypeError::class);
+
+        new WordWrapper(false);
+    }
+
+    final public function testWordWrapperShouldWrapWordsToLimitWhenGivenLimit()
+    {
+        $wordWrapper = new WordWrapper();
+        $actual = $wordWrapper->wrap('These are some words');
+
+        $expected = <<<TXT
+These
+ are 
+ some 
+words
+TXT;
+        self::assertEquals($expected, $actual);
     }
 }
