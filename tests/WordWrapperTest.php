7bd9d4e801bf3654de9566619fbcd477a132483b
Ben Peachey <b.peachey@dealerdirect.nl>
2016-10-27 13:56:03 +0200

    ADD: Adds empty test case.

---
+++ tests/WordWrapperTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Potherca\TestWorkshop;
+
+class WordWrapperTest extends \PHPUnit_Framework_TestCase
+{
+    final public function testNothing()
+    {
+        static::assertTrue(true);
+    }
+}
